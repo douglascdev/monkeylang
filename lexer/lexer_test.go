05de1ade2bd7361be21695c355deaa6c1ae787de
douglascdev <douglasc.dev@gmail.com>
2025-08-11 11:51:28 -0300

    More lexer stuff

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -10,7 +10,7 @@ func TestNextToken(t *testing.T) {
 	input := `=+{},;`
 
 	tests := []struct {
-		expectedTypekenType
+		expectedType          token.TokenType
 		expectedLiteral string
 	}{
 		{token.ASSIGN, "="},
@@ -18,4 +18,16 @@ func TestNextToken(t *testing.T) {
 	}
 
 	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+	}
 }
