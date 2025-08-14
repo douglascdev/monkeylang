33a1debd44fcf00d3a61f215cbcf4f0cacffba42
douglascdev <douglasc.dev@gmail.com>
2025-08-14 17:55:14 -0300

    Implement ast String() functions

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -4,6 +4,7 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/parser"
+	"monkey/token"
 	"testing"
 )
 
@@ -105,5 +106,28 @@ return 993322;
 		if returnStmt.TokenLiteral() != "return" {
 			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
 		}
+	}
+}
+
+func TestString(t *testing.T) {
+
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
